936fd3893827af3637d74e2156bf655f4e23d7a3
Stefan <111599445+bliek79@users.noreply.github.com>
2026-09-21 13:59:52 +0200

    Release Alpha7.23 version

--- custom_components/doems/const.py
+++ custom_components/doems/const.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 DOMAIN = "doems"
 NAME = "DOEMS"
-VERSION = "0.1.0-alpha.7.22"
+VERSION = "0.1.0-alpha.7.23"
 
 CONF_INSTANCE_NAME = "instance_name"
 DEFAULT_INSTANCE_NAME = NAME
